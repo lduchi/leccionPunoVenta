8aaba7b9048bbbad2ffb7ae63d36110b8ab6b75e
Luis Duchi <luis.duchi.est@tecazuay.edu.ec>
2023-12-07 11:19:33 -0500

    services

--- src/java/ws/servicios.java
+++ src/java/ws/servicios.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/WebService.java to edit this template
- */
+
 package ws;
 
+import java.util.ArrayList;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import modelo.Competencia;
+import modelo.Rol;
 
 /**
  *
@@ -21,5 +21,52 @@ public class servicios {
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "siexisterol")
+    public Boolean siexisterol(@WebParam(name = "nombre") String nombre) {
+        Rol rol = new Rol();
+        
+        ArrayList<Rol> rolesexistentes = rol.getRoles();
+        
+        for (Rol rols: rolesexistentes) {
+            if (rols.getRol().equals(nombre)) {
+                return true;
+            }
+            
+        }
+        return false;
+    }
+
+    
+    
+    
+    @WebMethod(operationName = "estadorol")
+    public Boolean estadorol(@WebParam(name = "nombrerol") String nombrerol) {
+        Rol rol = new Rol();
+        
+        ArrayList<Rol> estadospornombre = rol.getRoles();
+        
+        for (Rol rols: estadospornombre) {
+            if (rols.getRol().equals(nombrerol)) {
+                return rols.isEstado();
+            }
+            
+        }
+        return null;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "operation")
+    public Boolean operation(@WebParam(name = "nombre") String nombre) {
+        Competencia compe =  new Competencia();
+        ArrayList<Competencia> competenciaexiste = compe.getCompetencias();
+        
+        return null;
     }
 }
